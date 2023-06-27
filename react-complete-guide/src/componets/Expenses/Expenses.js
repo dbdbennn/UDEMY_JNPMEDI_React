e9afc90397534c3ad7f999d7c37269521995f2ac
Yu Jung Eun <0hh.0553@gmail.com>
2023-06-27 19:28:21 +0900

    sct4-73 정답

--- react-complete-guide/src/componets/Expenses/Expenses.js
+++ react-complete-guide/src/componets/Expenses/Expenses.js
@@ -11,6 +11,10 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filterExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
@@ -18,18 +22,14 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {props.items
-          .filter((expense) => {
-            return expense.date.getFullYear().toString() === filteredYear;
-          })
-          .map((expense) => (
-            <ExpenseItem
-              key={expense.id}
-              title={expense.title}
-              amount={expense.amount}
-              date={expense.date}
-            />
-          ))}
+        {filterExpenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
       </Card>
     </div>
   );
