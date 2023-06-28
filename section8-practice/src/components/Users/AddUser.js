3dab9afbeb034ea19723daabdc9cf620c182fabc
Yu Jung Eun <0hh.0553@gmail.com>
2023-06-28 10:23:02 +0900

    sct8-100 css module 변경중

--- section8-practice/src/components/Users/AddUser.js
+++ section8-practice/src/components/Users/AddUser.js
@@ -1,4 +1,6 @@
 import React from "react";
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const addUserHandler = (event) => {
@@ -6,13 +8,15 @@ const AddUser = (props) => {
   };
 
   return (
-    <form onSubmit={addUserHandler}>
-      <lable htmlFor="username">Username</lable>
-      <input id="username" type="text" />
-      <lable htmlFor="username">Age (Years)</lable>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card className={classes.input}>
+      <form onSubmit={addUserHandler}>
+        <lable htmlFor="username">Username</lable>
+        <input id="username" type="text" />
+        <lable htmlFor="username">Age (Years)</lable>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 
