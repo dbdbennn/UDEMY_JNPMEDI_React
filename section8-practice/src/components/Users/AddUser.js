ecbfa94d60a94c54b9667db8dd3bee4e09bf1ac8
Yu Jung Eun <0hh.0553@gmail.com>
2023-06-28 10:17:53 +0900

    sct8-99 AddUser

---
+++ section8-practice/src/components/Users/AddUser.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const AddUser = (props) => {
+  const addUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form onSubmit={addUserHandler}>
+      <lable htmlFor="username">Username</lable>
+      <input id="username" type="text" />
+      <lable htmlFor="username">Age (Years)</lable>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
