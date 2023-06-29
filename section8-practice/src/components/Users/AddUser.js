71ea74fb0581c094853781a2b7c8b75bc6fd8d65
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-06-29 10:43:55 +0900

    sct8-105 UserList 완료

--- section8-practice/src/components/Users/AddUser.js
+++ section8-practice/src/components/Users/AddUser.js
@@ -30,7 +30,7 @@ const AddUser = (props) => {
   };
 
   return (
-    <Card cssClass={classes.input}>
+    <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input
