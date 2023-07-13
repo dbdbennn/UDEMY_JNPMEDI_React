a28a0a750cc131db0f40b28f00a376f2c9d62180
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-07-13 10:06:39 +0900

    sct11-153 10:12

--- section11/src/store/CartProvider.js
+++ section11/src/store/CartProvider.js
@@ -1,12 +1,37 @@
+import { useReducer } from "react";
+
 import CartContext from "./cart-context";
 
+const defaultCartState = {
+  items: [],
+  totalAmout: 0,
+};
+
+const cartReducer = (state, action) => {
+  if (action.type === "ADD") {
+    const updateItems = state.items.concat(action.item);
+    const updateTotalAmount =
+      state.totalAmout + action.item.price * action.item.amount;
+    return {};
+  }
+  if (action.type === "REMOVE") return defaultCartState;
+};
+
 const CartProvider = (props) => {
-  const addItemToCartHandler = (item) => {};
-  const removeItemToCartHandler = (item) => {};
+  const [cartState, dispatchCartAction] = useReducer(
+    cartReducer,
+    defaultCartState
+  );
+  const addItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "ADD", item: item });
+  };
+  const removeItemToCartHandler = (item) => {
+    dispatchCartAction({ type: "REMOVE", item: item });
+  };
 
   const cartContext = {
-    items: [],
-    totalAmout: 0,
+    items: cartState.items,
+    totalAmout: cartState.totalAmout,
     addItem: addItemToCartHandler,
     removeItem: removeItemToCartHandler,
   };
