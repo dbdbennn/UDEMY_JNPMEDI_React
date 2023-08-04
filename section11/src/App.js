58faeda1acd8cdb6a7eeaedaa9855811d70f084a
Yu Jung Eun <85720276+dbdbennn@users.noreply.github.com>
2023-08-04 12:05:29 +0900

    folder name change

--- section11/src/App.js
+++ section11/src/App.js
@@ -1,29 +1,21 @@
-import React, { useState } from "react";
+import React, { useContext } from "react";
 
-import Header from "./components/Layout/Header";
-import Meals from "./components/Meals/Meals";
-import Cart from "./components/Cart/Cart";
-import CartProvider from "./store/CartProvider";
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
+import AuthContext from "./store/auth-context";
 
 function App() {
-  const [cartIsShown, setCartIsShown] = useState(false);
-
-  const showCartHandler = () => {
-    setCartIsShown(true);
-  };
-
-  const hideCartHandler = () => {
-    setCartIsShown(false);
-  };
+  const ctx = useContext(AuthContext);
 
   return (
-    <CartProvider>
-      {cartIsShown && <Cart onClose={hideCartHandler} />}
-      <Header onShowCart={showCartHandler} />
+    <React.Fragment>
+      <MainHeader />
       <main>
-        <Meals />
+        {!ctx.isLoggedIn && <Login />}
+        {ctx.isLoggedIn && <Home />}
       </main>
-    </CartProvider>
+    </React.Fragment>
   );
 }
 
